e4613d493803c2e56c9205a671e313cf3498989b
siliconninja <theninja@thesilicon.ninja>
2021-03-23 21:02:59 -0400

    Try Maven wooo

---
+++ src/main/java/siliconninja/meetinggrabber/MeetingGrabber/MeetingGrabber.java
@@ -0,0 +1,12 @@
+package siliconninja.meetinggrabber.MeetingGrabber;
+import net.md_5.bungee.api.plugin.Plugin;
+
+
+public class MeetingGrabber extends Plugin
+{
+    public static void main( String[] args )
+    {
+    	// https://www.spigotmc.org/wiki/bukkit-bungee-plugin-messaging-channel/
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/siliconninja/meetinggrabber/MeetingGrabber/AppTest.java
@@ -0,0 +1,20 @@
+package siliconninja.meetinggrabber.MeetingGrabber;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
